b92e71bd7067b2346a97ae8a70ed04b4232d3eca
sapna003 <sapnasahu32000@gmail.com>
2023-01-24 09:37:24 +0530

    my Project

---
+++ log_.js
@@ -0,0 +1,25 @@
+//when contact on main all  page code of bottom  
+
+function sub(){
+	alert("Thank your to join our  coommunity🙏 ");
+}
+
+//Registration form  page code 
+
+
+function sub2(){
+	alert("Your application is submitted Successfully");
+}
+
+
+
+//Appointment status page code 
+	
+
+	function status(){
+		alert("Your Appointment Successful Done Details Send on Your Mo. Number");
+	}
+ 
+
+
+	
